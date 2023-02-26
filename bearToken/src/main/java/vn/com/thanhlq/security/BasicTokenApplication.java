e03f967349da55aaf486e7bd7a34dc76a7e7c410
Thanh0408 <quangthanh.hust@gmail.com>
2023-02-26 22:36:31 +0700

    add Bear Token project

---
+++ bearToken/src/main/java/vn/com/thanhlq/security/BasicTokenApplication.java
@@ -0,0 +1,13 @@
+package vn.com.thanhlq.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTokenApplication.class, args);
+	}
+
+}
